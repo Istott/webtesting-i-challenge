eedc34a22328852f64d05521d2d9a062c95fa6ba
isaac stott <isaac.cloyd@gmail.com>
2020-06-17 16:52:10 -0600

    wrote up enhancer and working on tests

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -2,21 +2,41 @@ module.exports = {
   succeed,
   fail,
   repair,
-  get,
+  // get,
 };
 
 function succeed(item) {
+  item.enhancement++
+
+  if(item.enhancement >= 20){
+    return item.enchancement === 20;
+  }
+
   return { ...item };
 }
 
 function fail(item) {
+  if(item.enhancement < 15){
+    return item.durablility - 5;
+  } else if(item.enhancement >= 15){
+    return item.durability - 10;
+  }
+  
+  if(item.enhancement > 16){
+    return item.enhancement - 1;
+  }
+
   return { ...item };
 }
 
 function repair(item) {
+  if(item.durability === 0) {
+    return item.durability === 100;
+  }
+
   return { ...item };
 }
 
-function get(item) {
-  return { ...item };
-}
+// function get(item) {
+//   return { ...item };
+// }
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,35 @@
-const enhancer = require('./enhancer.js');
+// const enhancer = require('./enhancer.js');
+const {succeed, fail, repair} = require('./enhancer.js');
+
 // test away!
+
+test('testing', () => {
+    expect(1 + 2).toEqual(3);
+})
+
+describe("enhancer", () => {
+    test('should run tests using it()', () => {
+        expect(true).toBe(true);
+        expect(true).toBeTruthy();
+        expect(true).not.toBeUndefined();
+    })
+
+
+    describe('succeed()', () => {
+        test("should increment enhancement by 1", () => {
+            expect(succeed).toBeTruthy();
+        })
+    })
+
+    describe('fail()', () => {
+        test('should decrement enhancement level and durability', () => {
+           const durability = 100;
+           const  enhancement = 20;
+
+            
+            expect(fail(durability - 10)).toBe(90);
+            expect(fail(enhancement - 1)).toBe(19);
+        })
+    })
+
+})
